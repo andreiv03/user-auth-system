71aae21c611eb5f30bd92714d925f28871cff0c6
Andrei Voicu <andrei.voicu110@gmail.com>
2022-01-23 15:34:23 +0200

    Update

--- middleware/authorization.ts
+++ middleware/authorization.ts
@@ -1,17 +1,16 @@
 import type { NextApiRequest } from "next";
-
-import UsersModel from "../models/users-model";
-import Token from "../utils/token";
 import { ADMINS } from "../constants";
 
 const Authorization = async (req: NextApiRequest, adminRequired: boolean) => {
   const authorization = req.headers.authorization;
   if (!authorization) throw new Error("Unauthorized!");
 
+  const { default: Token } = await import("../utils/token");
   const decoded = await Token.verifyToken(authorization).catch(() => {
     throw new Error("Unauthorized!");
   });
 
+  const { default: UsersModel } = await import("../models/users-model");
   const user = await UsersModel.findById(decoded.sub).select("firstName lastName email phoneNumber avatar").select("-password").lean();
   if (!user) throw new Error("User not found!");
 
